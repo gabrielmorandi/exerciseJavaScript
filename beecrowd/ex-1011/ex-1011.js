ad86a18717fb308c92f36be78b48325a9f46f8b2
GabrielMorandi <morandi67766@gmail.com>
2022-10-29 03:31:09 -0300

    up

---
+++ beecrowd/ex-1011/ex-1011.js
@@ -0,0 +1,13 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split('\n');
+
+function CalcularEsfera(raio){
+    const pi = 3.14159
+    var raio = parseInt(lines[0])
+
+    esfera = ((4.0 / 3.0) * pi * raio ** 3).toFixed(3)
+
+    console.log(`VOLUME = ${esfera}`);
+}
+
+CalcularEsfera()
